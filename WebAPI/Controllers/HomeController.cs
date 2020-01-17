a1b9f8e5936dd4fbe2dec8220f365e342b143779
MateuszK <mateusz.konopka11@gmail.com>
2020-01-17 20:06:24 +0100

    Models folder and HomeController added.

---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly AuthContext _context;
+
+        public HomeController(AuthContext context)
+        {
+            _context = context;
+        }
+    }
+}
